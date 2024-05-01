c5e2f2c8cbe2aece70b13022ab6cef8694c7aa8f
Matthew Stefanovic <ms@stefanovic.us>
2024-04-30 21:04:10 -0400

    Can filter for email body only

--- backend/import_data.py
+++ backend/import_data.py
@@ -1,42 +1,44 @@
 import glob
 import rich.progress
 import logging
+import mailparser
+import chardet
 from rich.logging import RichHandler
 from rich import print
-from talon import signature
-from talon.quotation import extract_from_plain
 
 # Setup rich handler for better logging experience
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.ERROR,
     format="%(message)s",
     datefmt="[%X]",
     handlers=[RichHandler()]
 )
 log = logging.getLogger("rich")
 
+def detect_encoding(file_path):
+    with open(file_path, 'rb') as file:
+        raw_data = file.read()
+    result = chardet.detect(raw_data)
+    return result['encoding']
+
 def clean_email(file_path):
-    # Read the email content from the file
     log.info(f'Cleaning {file_path}')
-    with open(file_path, 'r', encoding='utf-8') as file:
-        email_text = file.read()
-    print(email_text)
-
-    # Use Talon to extract the body and signature
-    body, signature = signature.extract_signature(email_text)
-
-    # Optionally, remove quoted text
-    clean_body = extract_from_plain(body)
-
-    return clean_body
+    encoding = detect_encoding(file_path)
+    try:
+        with open(file_path, 'r', encoding=encoding) as file:
+            email_text = file.read()
+        email_text = email_text.replace('\\', '\\\\')
+        email = mailparser.parse_from_string(email_text)
+        return email.body
+    except Exception as e:
+        log.exception(f'Error processing {file_path}: {e}')
+        return None
 
 log.info('Compiling email files')
 email_files = list(glob.glob('./dataset/maildir/**/*.', recursive=True))
-email_files[:10]
+print(len(email_files))
 with rich.progress.Progress() as progress:
     task = progress.add_task('[cyan]Adding emails to vector database...', total=len(email_files))
     for file in email_files:
         email = clean_email(file)
-        print(email)
-        print()
         progress.update(task, advance=1)
