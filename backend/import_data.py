e1c1e8aebabfb733dbbf79082a913152d458127a
Matthew Stefanovic <ms@stefanovic.us>
2024-04-30 23:07:36 -0400

    Emails added to vector db

--- backend/import_data.py
+++ backend/import_data.py
@@ -1,16 +1,24 @@
+import chardet
+import faiss
 import glob
 import logging
 import mailparser
-import chardet
+import numpy as np 
+import random
 import re
-import datetime
-from rich.progress import track
-from rich.logging import RichHandler
+import os
+import sys
+
+from dateutil import parser
 from rich import print
+from rich.logging import RichHandler
+from rich.progress import track
+from sentence_transformers import SentenceTransformer
 
 # Setup rich handler for better logging experience
+logging.getLogger("transformers").setLevel(logging.WARNING)
 logging.basicConfig(
-    level=logging.ERROR,
+    level=logging.INFO,
     format="%(message)s",
     datefmt="[%X]",
     handlers=[RichHandler()]
@@ -24,7 +32,7 @@ def detect_encoding(file_path):
     return result['encoding']
 
 def clean_email(file_path):
-    log.info(f'Cleaning {file_path}')
+    log.debug(f'Cleaning {file_path}')
     encoding = detect_encoding(file_path)
     try:
         with open(file_path, 'r', encoding=encoding) as file:
@@ -38,10 +46,11 @@ def clean_email(file_path):
 
 def parse_date(date_str):
     try:
-        date_str = date_str.split(' (')[0]
-        return datetime.datetime.strptime(date_str, '%a, %d %b %Y %H:%M:%S %z')
+        # Strip off common trailing issues such as 'GMT' and 'Subject:'
+        clean_str = re.sub(r'(\sGMT)?\s*Subject:.*', '', date_str)
+        return parser.parse(clean_str)
     except ValueError as e:
-        log.warning(f"Date parsing error: {e}")
+        log.warning(f"Date parsing error: {e} for date string: {date_str}")
         return None
 
 def parse_email_list(emails):
@@ -85,25 +94,21 @@ def extract_metadata(email_text):
         else:
             buffer.append(line)
 
-    if current_key:
-        value = ' '.join(buffer).strip()
-        if current_key in ['Date']:
-            metadata[current_key] = parse_date(value)
-        elif current_key in ['To', 'Cc', 'Bcc']:
-            metadata[current_key] = parse_email_list(value)
-        elif current_key in ['Content-Type']:
-            metadata[current_key] = parse_complex_field(value)
-        else:
-            metadata[current_key] = value
-
     return metadata
 
 def get_metadata(file_path):
-    log.info(f'Getting metadata for {file_path}')
+    log.debug(f'Getting metadata for {file_path}')
     encoding = detect_encoding(file_path)
     try:
         with open(file_path, 'r', encoding=encoding) as file:
-            email_text = file.read()
+            # Read the first 15 lines
+            lines = []
+            for _ in range(15):
+                line = file.readline()
+                if not line:
+                    break  # End of file reached
+                lines.append(line)
+            email_text = ''.join(lines)
         metadata = extract_metadata(email_text)
         return metadata
     except Exception as e:
@@ -111,10 +116,38 @@ def get_metadata(file_path):
         return None
 
 
-log.info('Compiling email files')
-email_files = list(glob.glob('./dataset/maildir/**/*.', recursive=True))
-email_files = email_files[:50]
-for file in track(email_files, description='[cyan]Adding emails to vector database...'):
-    email = clean_email(file)
-    metadata = get_metadata(file)
-    print(metadata)
+def create_vector_db():
+    log.info('Compiling email files')
+    email_files = list(glob.glob('./dataset/maildir/**/*.', recursive=True))
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    # Initialize a FAISS index
+    log.info('Initialize a FAISS index')
+    d = 384  # Dimension of vectors, change based on the model used
+    faiss_index = faiss.IndexFlatL2(d)  # Using IndexFlatL2 for simplicity
+
+    num_to_process = 1000
+    files_to_process = random.sample(email_files, num_to_process)
+
+    log.info('Adding emails to vector database')
+    file_ids = {}
+    for index, file in enumerate(track(files_to_process, description='[cyan]Adding emails to vector database...')):
+        email = clean_email(file)
+        metadata = get_metadata(file)
+        metadata['File'] = file
+        file_ids[index] = metadata
+
+        if email is not None:
+            original_stdout = sys.stdout
+            sys.stdout = open(os.devnull, 'w')
+
+            vector = model.encode([email])[0]
+            faiss_index.add(np.array([vector]))
+
+            sys.stdout = original_stdout
+
+
+    return faiss_index, file_ids
+
+if __name__ == "__main__":
+    create_vector_db()
